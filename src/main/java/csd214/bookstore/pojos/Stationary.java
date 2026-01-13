79713c2163529d78cf26f35bc98cff6bd4a08eb0
batista-s <batistanostalgia@gmail.com>
2026-01-13 13:22:14 -0500

    add stationary

---
+++ src/main/java/csd214/bookstore/pojos/Stationary.java
@@ -0,0 +1,48 @@
+package csd214.bookstore.pojos;
+
+import java.util.Objects;
+
+public abstract class Stationary extends Product {
+    private String brand;
+
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public void initialize(){
+        System.out.println("Enter Brand:");
+        this.brand = getInput("Generic");
+    }
+
+    @Override
+    public void edit() {
+        // 2. Edit Self fields
+        System.out.println("Edit Stationary [" + this.brand + "]:");
+
+        // These two line do the same thing
+        // this.brand = getInput(this.brand);
+        setBrand(getInput("Generic"));
+    }
+
+    @Override
+    public String toString() {
+        return "Stationary{" +
+                "brand='" + brand + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Stationary that)) return false;
+        return Objects.equals(getBrand(), that.getBrand());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getBrand());
+    }
+}
